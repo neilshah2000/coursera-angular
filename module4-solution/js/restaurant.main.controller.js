5c9db2442b5cb330f64a02c34b93492dbc3d27f9
neilshah2000 <neil.shah@thomsonreuters.com>
2016-10-06 14:18:57 +0100

    ass 4

---
+++ module4-solution/js/restaurant.main.controller.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict';
+
+  angular.module('restaurant')
+  .controller('MainRestaurantController', MainRestaurantController);
+
+  function MainRestaurantController(){
+    var restaurant;
+    restaurant = this;
+    
+  }
+})();
